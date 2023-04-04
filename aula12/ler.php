0ff01a3a263c91181f03c306a7244b7ba1e37118
Milhorini777 <samuel.milhorini@alunos.ifsuldeminas.edu.br>
2023-04-04 08:22:03 -0300

    .

---
+++ aula12/ler.php
@@ -0,0 +1,10 @@
+<?php include_once "header.php" ?>
+<?php include_once "menu.php" ?>
+
+<div class="contaier">
+    <h1>Título</h1>
+    <img src="" clas=""/>
+    <p>Texto da notícia</p>
+</div>
+
+<?php include_once "rodape.php" ?>
