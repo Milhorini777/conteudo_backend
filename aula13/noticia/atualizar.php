63540d333c24df142b5cf9c15acf2ec65af0cc55
Milhorini777 <samuel.milhorini@alunos.ifsuldeminas.edu.br>
2023-05-23 08:42:47 -0300

    aula13

---
+++ aula13/noticia/atualizar.php
@@ -0,0 +1,33 @@
+<?php
+   
+   //importa o arquivo de conexão
+   require_once "../banco/conexao.php";
+
+   $id = $_POST['idnoticia'];
+   $titulo = $_POST['titulo'];
+   $materia = $_POST['materia'];
+   $categoria = $_POST['categoria'];
+
+   //cria uma variável com um comando SQL
+   $SQL = "UPDATE `noticia` SET `titulo`= ?, `materia`= ?, `categoria`= ? WHERE  `idnoticia`= ? ;";
+ 
+   //prepara o comando para ser executado no mysql
+   $comando = $conexao->prepare($SQL);
+
+   //faz a vinculação dos parâmetros ?, ?, ?
+   $comando->bind_param("sssi", $titulo, $materia, $categoria, $id);
+
+   //executa o comando
+   $comando->execute();
+
+   //volta para o formulário aaaaaaa
+   header("Location: index.php");
+
+   
+
+
+
+
+
+
+
---
+++ aula13/noticia/consultar_todos.php
@@ -0,0 +1,27 @@
+<?php
+   
+   //importa o arquivo de conexão
+   require_once "../banco/conexao.php";
+
+   //cria uma variável com um comando SQL
+   $SQL = "SELECT * FROM noticia";
+ 
+   //prepara o comando para ser executado no mysql
+   $comando = $conexao->prepare($SQL);
+
+   
+   //executa o comando
+   $comando->execute();
+
+   //pegar os resultados da consulta - todas as linhas de resultados
+   $resultados = $comando->get_result();
+
+   //pega a primeira linha do resultado da consulta
+   $usuarios = [];
+   while ($usuario = $resultados->fetch_object()){
+
+      $usuarios[] = $usuario;
+   }
+
+
+   
